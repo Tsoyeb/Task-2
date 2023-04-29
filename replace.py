cfd7cc7e11cc2796dbaa42060539c7c2e86571b2
Tabassum <127255282+Tsoyeb@users.noreply.github.com>
2023-04-29 11:11:59 +0100

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,12 @@
+# Task 2 
+# Use Replace function to substitute ! with a space. 
+sentence = 'The!quick!brown!fox!jumps!over!the!lazy!dog!'
+sentence_2 = sentence.replace('!',' ')
+print(sentence_2)
+
+# Reprint sentence as Capitals 
+sentence_3 = sentence_2.upper()
+print(sentence_3)
+
+# Print the sentence in reverse
+print(sentence_3[::-1])
---
+++ strings.py
@@ -0,0 +1,4 @@
+# Task 1 to strip $ from Superman string 
+hero = '$$$Superman$$$'
+strip_hero = hero.strip('$')
+print(strip_hero)
